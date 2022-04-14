f60691c1bbefd3f0a3b9bfa2613a63800413fad9
nikita <nikita-agarwal21>
2022-04-15 00:25:01 +0530

    second commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
     def index
-        @movies=["hera pheri","hey baby","de dana dan"]
+      @movies=["hera pheri","hey baby","de dana dan"]
     end
 end
